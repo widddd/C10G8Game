b4605c4aa216d0dece4e5ba8f2928b7f2a2b35a3
BuildTools <unconfigured@null.spigotmc.org>
2023-10-05 00:19:14 +0800

    EARLY TEST VERSION 0.2

---
+++ C10G8Game/mainfunc.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+//函数声明
+void debug_start();//调试程序
+void debug_output1();//调试元组输出
+void background1();//故事背景
+void opening();//序章：风雨之路
+int FirstChoice0();//看着
+int FirstChoice1();//给伞
+int FirstChoice2();//一起撑伞
+void ShowScore();//显示分数
+void AddScore(int, int, int, int);//加分函数
+void SceneEllaSlip();//班花摔倒
+int LiftUpElla0();//不扶她
+int LiftUpElla1();//扶她起来
+int BackToClass();//回教室剧情
+int HandOutWithElla0();//坐着
+int HandOutWithElla1();//和她一起发
+int HandToElla0();//给她卷子
+int HandToElla1();//把卷子藏起来
+int opening_ending();//序章谢幕
